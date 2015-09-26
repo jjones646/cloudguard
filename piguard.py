e3ee56693d36e9d60f28bab7edead249198ec602
Jon <jjones646@gmail.com>
2015-09-26 19:54:58 -0400

    improving piguard script

--- piguard.py
+++ piguard.py
@@ -174,7 +174,6 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 
     # draw the text and timestamp on the frame
     ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-    ts_iso = timestamp.strftime('%Y-%m-%d %H:%M:%S')
     ts_utc = datetime.utcnow()
 
     cv2.putText(frame, "Room Status: {}".format(
@@ -214,13 +213,13 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
             # save image to the liveview frame
             cv2.imwrite(liveview_filename, frame)
             # give some feedback on the console
-            print logc.INFO + "[SAVE]" + logc.ENDC, "[" + str(ts_utc) + "]", "liveview frame updated"
+            print logc.INFO + "[SAVE]" + logc.ENDC, "[" + ts_utc + "]", "liveview frame updated"
 
         if conf["log_motion"]:
             # store the timestamp into a json log file
             log_entry = {}
             log_entry["motion_count"] = motionCounter
-            log_entry["ts"] = ts_iso
+            log_entry["ts"] = ts_utc
 
             with open(liveview_log) as f:
                 try:
