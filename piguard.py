e22b1d0b2f2ab4e6a3a160a6f7c1f46af1ccb377
Jon <jjones646@gmail.com>
2015-09-26 18:45:10 -0400

    improving piguard script

--- piguard.py
+++ piguard.py
@@ -47,7 +47,7 @@ if conf["use_dropbox"]:
  
     # connect to dropbox and start the session authorization process
     flow = DropboxOAuth2FlowNoRedirect(conf["dropbox_key"], conf["dropbox_secret"])
-    print "[INFO] Authorize this application: {}".format(flow.start())
+    print logc.INFO + "[INFO]" + logc.ENDC, "Authorize this application: {}".format(flow.start())
     authCode = raw_input("Enter auth code here: ").strip()
  
     # finish the authorization and grab the Dropbox client
@@ -64,9 +64,9 @@ rawCapture = PiRGBArray(camera, size=camera.resolution)
 rawCapture.truncate(0)  # clear out the buffer before its used
 
 # show OpenCV version information
-print bcolors.INFO + "[INFO]" + bcolors.ENDC, "OpenCV version:\t%s" % cv2.__version__
+print logc.INFO + "[INFO]" + logc.ENDC, "OpenCV version:\t%s" % cv2.__version__
 # show what resolution we're using
-print bcolors.INFO + "[INFO]" + bcolors.ENDC, "Camera res.:\t%dx%d" % tuple(conf["resolution"])
+print logc.INFO + "[INFO]" + logc.ENDC, "Camera res.:\t%dx%d" % tuple(conf["resolution"])
 
 # blink the camera's LED to show we're starting up
 try:
