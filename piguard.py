4db78754a64393c8f4da1fa07441f7d6b7e44dae
Jon <jjones646@gmail.com>
2015-09-26 18:24:22 -0400

    improving piguard script

--- piguard.py
+++ piguard.py
@@ -80,6 +80,7 @@ except:
  
 # show detailed OpenCV version information
 build_info = cv2.getBuildInformation()
+build_info_split = [s.strip() for s in build_info.splitlines()]
 print type(build_info)
 pprint.pprint(locals())
 #print "[INFO] OpenCV version:", build_info
