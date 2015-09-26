55b16ea95d0ff9d9f203697584fb4a3e8d39e95e
Jon <jjones646@gmail.com>
2015-09-26 18:40:13 -0400

    improving piguard script

--- bcolors.py
+++ bcolors.py
@@ -1,6 +1,6 @@
 class bcolors:
-	def __init__(self):
-		self.HEADER = ''
+    def __init__(self):
+        self.HEADER = ''
         self.INFO = ''
         self.OK = ''
         self.WARN = ''
@@ -9,18 +9,18 @@ class bcolors:
         self.BOLD = ''
         self.UNDERLINE = ''
 
-	def enable(self):
-	    self.HEADER = '\033[95m'
-	    self.INFO = '\033[94m'
-	    self.OK = '\033[92m'
-	    self.WARN = '\033[93m'
-	    self.FAIL = '\033[91m'
-	    self.ENDC = '\033[0m'
-	    self.BOLD = '\033[1m'
-	    self.UNDERLINE = '\033[4m'
+    def enable(self):
+        self.HEADER = '\033[95m'
+        self.INFO = '\033[94m'
+        self.OK = '\033[92m'
+        self.WARN = '\033[93m'
+        self.FAIL = '\033[91m'
+        self.ENDC = '\033[0m'
+        self.BOLD = '\033[1m'
+        self.UNDERLINE = '\033[4m'
 
-	def disable(self):
-		self.HEADER = ''
+    def disable(self):
+        self.HEADER = ''
         self.INFO = ''
         self.OK = ''
         self.WARN = ''
