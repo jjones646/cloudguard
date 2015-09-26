ae4ffd076b079f90ad35859f80cf0e0cbb8e86a6
Jon <jjones646@gmail.com>
2015-09-26 02:46:39 -0400

    fixing all tabs

--- piguard.py
+++ piguard.py
@@ -21,11 +21,11 @@ args = vars(ap.parse_args())
 warnings.filterwarnings("ignore")
 conf = json.load(open(args["conf"]))
 client = None
-
+ 
 if conf["use_dropbox"]:
     from dropbox.client import DropboxOAuth2FlowNoRedirect
     from dropbox.client import DropboxClient
-
+ 
     # connect to dropbox and start the session authorization process
     flow = DropboxOAuth2FlowNoRedirect(conf["dropbox_key"], conf["dropbox_secret"])
     print "[INFO] Authorize this application: {}".format(flow.start())
@@ -35,7 +35,7 @@ if conf["use_dropbox"]:
     (accessToken, userID) = flow.finish(authCode)
     client = DropboxClient(accessToken)
     print "[SUCCESS] dropbox account linked"
-
+ 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = tuple(conf["resolution"])
@@ -43,7 +43,7 @@ camera.framerate = conf["fps"]
 rawCapture = PiRGBArray(camera, size=camera.resolution)
 # clear out the buffer before its used
 rawCapture.truncate(0)
-
+ 
 try:
     for i in range(10):
         camera.led = True
@@ -52,9 +52,9 @@ try:
         time.sleep(0.05)
 except:
     pass
-
+ 
 print "--  resolution: ", "%dx%x" % tuple(conf["resolution"])
-
+ 
 # allow the camera to warmup, then initialize the average frame, last
 # uploaded timestamp, and frame motion counter
 print "[INFO] warming up..."
@@ -64,11 +64,11 @@ avg = None
 lastUploaded = datetime.datetime.now()
 motionCounter = 0
 ledState = False
-
+ 
 # create a GUI window
 cv2.namedWindow("PiGuard")
 cv2.setWindowProperty("PiGuard", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
-
+ 
 # capture frames from the camera
 for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     try:
@@ -76,14 +76,14 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
         ledState = not ledState
     except:
         pass
-
+ 
     # grab the raw NumPy array representing the image and initialize
     # the timestamp and occupied/unoccupied text
     frame = f.array
-
+ 
     timestamp = datetime.datetime.now()
     text = "Unoccupied"
-
+ 
     # resize the frame, convert it to grayscale, and blur it
     frame = imutils.resize(frame, width=500)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -101,7 +101,7 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
     # frame and running average
     cv2.accumulateWeighted(gray, avg, 0.5)
     frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-
+ 
     # threshold the delta image, dilate the thresholded image to fill
     # in holes, then find contours on thresholded image
     thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
@@ -124,52 +124,52 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
     ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
     cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-
+ 
     # check to see if the room is occupied
     if text == "Occupied":
         # check to see if enough time has passed between uploads
         if (timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:
-            # increment the motion counter
-			motionCounter += 1
+        # increment the motion counter
+        motionCounter += 1
  
-			# check to see if the number of frames with consistent motion is
-			# high enough
-			if motionCounter >= conf["min_motion_frames"]:
+        # check to see if the number of frames with consistent motion is
+        # high enough
+        if motionCounter >= conf["min_motion_frames"]:
                 # check to see if dropbox sohuld be used
                 if conf["use_dropbox"]:
-                	# write the image to temporary file
-                	t = TempImage()
-                	cv2.imwrite(t.path, frame)
-
-                	# upload the image to Dropbox and cleanup the tempory image
-                	print "[UPLOAD] {}".format(ts)
-                	path = "{base_path}/{timestamp}.jpg".format(base_path=conf["dropbox_base_path"], timestamp=ts)
-                	client.put_file(path, open(t.path, "rb"))
-                	t.cleanup()
-
+                    # write the image to temporary file
+                    t = TempImage()
+                    cv2.imwrite(t.path, frame)
+ 
+                    # upload the image to Dropbox and cleanup the tempory image
+                    print "[UPLOAD] {}".format(ts)
+                    path = "{base_path}/{timestamp}.jpg".format(base_path=conf["dropbox_base_path"], timestamp=ts)
+                    client.put_file(path, open(t.path, "rb"))
+                    t.cleanup()
+ 
                 # see if we should save this locally
                 if conf["save_local"]:
                     cv2.imwrite(os.path.join(os.getcwd(), 'live-stream.jpg'), frame)
-
+ 
                 # update the last uploaded timestamp and reset the motion counter
                 lastUploaded = timestamp
                 motionCounter = 0
  
-	# otherwise, the room is not occupied
-	else:
-		motionCounter = 0
+    # otherwise, the room is not occupied
+    else:
+        motionCounter = 0
         # setup for next capture
         rawCapture.truncate(0)
-
-	# check to see if the frames should be displayed to screen
-	if conf["show_video"]:
-		cv2.imshow("PiGuard", frame)
-		key = cv2.waitKey(1) & 0xFF
-
-	# # if the `q` key is pressed, break from the lop
-	# if key == ord("q"):
-	# 	break
  
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
-
+    # check to see if the frames should be displayed to screen
+    if conf["show_video"]:
+        cv2.imshow("PiGuard", frame)
+        key = cv2.waitKey(1) & 0xFF
+ 
+        # # if the `q` key is pressed, break from the lop
+        # if key == ord("q"):
+        # 	break
+ 
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+ 
