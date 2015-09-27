d7b3c3375e99805d35b89202d260af1bf8f37645
Jon <jjones646@gmail.com>
2015-09-27 02:17:51 -0400

    fixing logs

--- logging.py
+++ logging.py
@@ -9,7 +9,7 @@ logc = logcolors.LogColors()
 
 def archive(filename):
     pre_move = abspath(filename)
-    post_move = abspath(join(pre_move, str(datetime.now().strftime("%Y-%m-%d_%H:%M:%S_")) + basename(filename)))
+    post_move = abspath(join(dirname(pre_move), str(datetime.now().strftime("%Y-%m-%d_%H:%M:%S_")) + basename(filename)))
     try:
         os.rename(pre_move, post_move)
     except:
