a897c3d70402e0d83c4e322c9d06be6196a9bd2a
Jon <jjones646@gmail.com>
2015-09-27 05:15:03 -0400

    fixing logs

--- piguard.py
+++ piguard.py
@@ -328,7 +328,7 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
     delta_ts = current_ts - last_motion_ts
     # append to back and pop from front
     moving_average_array.append(delta_ts.seconds)
-    moving_average_array.pop(0)
+    moving_average_array.pop()
     avg_delta_ts = sum(movingAverage(
         moving_average_array, window=conf["min_motion_frames"] * 4))
 
