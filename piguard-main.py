ca27094fdf68021d1e4178dd4d3d0cd3aeddb807
Jon <jjones646@gmail.com>
2015-11-18 22:46:51 -0500

    moving main file...again

--- piguard-main.py
+++ piguard-main.py
@@ -5,7 +5,6 @@ import imutils
 import cv2
 import imutils
 import datetime
-import cvfps
 import multiprocessing as mp
 import numpy as np
 from os.path import *
@@ -15,49 +14,51 @@ from multiprocessing.pool import ThreadPool
 from collections import deque
 from common import clock, draw_str, StatValue
 
-dimm = (640,480)
+rez = (640,480)
 fps = 15
+contourThresh = 2750
+bgSubHist = 200
+bgSubThresh = 16
 
-# first camera, with resolution specified above
+# this selects the first camera found camera
 cap = cv2.VideoCapture(-1)
 
-fpsPre = cap.get(cv2.CAP_PROP_FPS)
-cap.set(cv2.CAP_PROP_FPS, fps)
-fpsNow = cap.get(cv2.CAP_PROP_FPS)
+# set the framerate as specified at the top
+try:
+    cap.set(cv2.CAP_PROP_FPS, fps)
+except:
+    print "Unable to set framerate to {:.3f}!".format(fps)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print "--  using framerate: {:.3f}".format(fps)
 
-dimmPre = (cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, dimm[0])
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, dimm[1])
-dimmNow = (cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-print "Dimmensions:\t{}".format(dimmNow)
-print "FPS:\t\t{:.2f}".format(fpsNow)
+# set the resolution as specified at the top
+try:
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, rez[0])
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, rez[1])
+except:
+    print "Unable to set resolution to {0}x{0}!".format(*rez)
+    rez = (cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print "--  using resolution: {0}x{1}".format(*rez)
 
 # background subtractor
-fgbg = cv2.createBackgroundSubtractorMOG2(200, 14)
+fgbg = cv2.createBackgroundSubtractorMOG2(bgSubHist, bgSubThresh)
 
 # encoded file object
-vidStreamName = abspath(join(os.getcwd(), "/home/jonathan/Documents/piguard/liveview/vidStream.avi"))
-vidStream = cv2.VideoWriter(vidStreamName, cv2.VideoWriter_fourcc(*'XVID'), fps, dimm)
+vidStream_fn = abspath(join(dirname(realpath(__file__)), "liveview/vidStream.avi"))
+vidStream = cv2.VideoWriter(vidStream_fn, cv2.VideoWriter_fourcc(*'XVID'), fps, rez)
 
-# find the size to use for the gaussian blur
-blurSz = dimm[0]/30
-if blurSz % 2 == 0:
-    blurSz += 1
-blurSz = (blurSz, blurSz)
+# video window
+cv2.namedWindow('PiGuard')#, cv2.WINDOW_NORMAL)
 
-cv2.namedWindow('PiGuard', cv2.WINDOW_NORMAL)
-threaded_mode = True
-
+threadingEn = True
 threadN = mp.cpu_count()
+
 pool = ThreadPool(processes=threadN)
 pending = deque()
 
 latency = StatValue()
 frame_interval = StatValue()
 last_frame_time = clock()
-
-fpsTimer = cvfps.cvTimer(length=50, target_fps=fps)
 
 class DummyTask:
     def __init__(self, data):
@@ -76,7 +77,7 @@ def drawFrame(frame, rects, thickness=1, color=(127,127,127)):
     # loop over the contours
     for c in cnts:
         # if the contour is too small, ignore it
-        if cv2.contourArea(c) < 2500:
+        if cv2.contourArea(c) < contourThresh:
             continue
         box = cv2.boxPoints(cv2.minAreaRect(c))
         box = np.int0(box)
@@ -84,7 +85,7 @@ def drawFrame(frame, rects, thickness=1, color=(127,127,127)):
         detections += 1
     return frameFrames, detections
 
-def processFrame(frame, t0, timestamp, rotateAng=False, newWidth=False):
+def processFrame(frame, t0, ts, rotateAng=False, newWidth=False):
     if rotateAng is not False:
         frame = imutils.rotate(frame, angle=rotateAng)
     if newWidth is not False:
@@ -96,15 +97,13 @@ def processFrame(frame, t0, timestamp, rotateAng=False, newWidth=False):
     # bg sub
     fgmask = fgbg.apply(frame)
     # get our frame outlines
-    frame, moveCount = drawFrame(frame, fgmask, thickness=2)
+    frame, det = drawFrame(frame, fgmask, thickness=2)
     # return immediately if there's no motion
-    if moveCount == 0:
-        return frameBak, frame, t0, moveCount, timestamp
+    if det != 0:
+        frame = cv2.add(frame, detectPerson(frameBak))
+        frame = cv2.add(frame, detectFace(frameBak))
+    return frameBak, frame, t0, det, ts
 
-    frame = cv2.add(frame, detectPerson(frameBak))
-    frame = cv2.add(frame, detectFace(frameBak))
-
-    return frameBak, frame, t0, moveCount, timestamp
 
 while True:
     while len(pending) > 0 and pending[0].ready():
@@ -119,21 +118,25 @@ while True:
             frameBg = cv2.bitwise_and(roi, roi, mask=cv2.bitwise_not(frameMask))
             frameMaskFg = cv2.bitwise_and(frameDraw, frameDraw, mask=frameMask)
             frame[0:sz[1], 0:sz[1]] = cv2.add(frameBg, frameMaskFg)
-            # put the time on our frame
-            cv2.putText(
-                    frame,
-                    "{}".format(ts),
-                    (10, frame.shape[0] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.55,
-                    (0,0,255),
-                    1
-            )
-            # overlay stats
-            draw_str(frame, (20, 20), "threaded:       {} ({})".format(threaded_mode, threadN))
-            draw_str(frame, (20, 38), "latency:        {:.1f}ms".format(latency.value*1000))
-            draw_str(frame, (20, 56), "frame interval: {:.1f}ms".format(frame_interval.value*1000))
-            cv2.imshow('PiGuard', frame)
+        # put the time on our frame
+        cv2.putText(
+                frame,
+                "{}".format(ts),
+                (10, frame.shape[0] - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.55,
+                (0,0,255),
+                1
+        )
+        if threadingEn is False:
+            threadDis = 1
+        else:
+            threadDis = threadN
+        # overlay some stats
+        draw_str(frame, (20, 20), "threads:        {}".format(threadDis))
+        draw_str(frame, (20, 38), "latency:        {:.1f}ms".format(latency.value*1000))
+        draw_str(frame, (20, 56), "frame interval: {:.1f}ms".format(frame_interval.value*1000))
+        cv2.imshow('PiGuard', frame)
 
     if len(pending) < threadN:
         ret, frame = cap.read()
@@ -141,7 +144,7 @@ while True:
         frame_interval.update(t - last_frame_time)
         last_frame_time = t
         ts = datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p")
-        if threaded_mode:
+        if threadingEn:
             task = pool.apply_async(processFrame, args=(frame.copy(), t, ts))
         else:
             task = DummyTask(processFrame(frame, t, ts))
@@ -150,7 +153,7 @@ while True:
 
     ch = 0xFF & cv2.waitKey(1)
     if ch == ord(' '):
-        threaded_mode = not threaded_mode
+        threadingEn = not threadingEn
     if ch == 27:
         break
 
